2c20a6d6e6ab83a89962b08e35dd3123ae5909e6
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-27 07:48:41 -0600

    ads try catch to main to catch radius error

--- src/main/java/com/los/shapes/Main.java
+++ src/main/java/com/los/shapes/Main.java
@@ -2,7 +2,12 @@ package com.los.shapes;
 
 public class Main {
     public static void main(String[] args) {
-        Circle c1 = new Circle(0, 0, 0);
-        System.out.println(c1);
+        try {
+            Circle c1 = new Circle(0, 0, 0);
+            System.out.println(c1);
+        } catch( IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
